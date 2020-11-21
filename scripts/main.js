787867c4baddc623928796c3fc02f4a619cfe86f
Giorgio Sawaya <giorgiosawaya@gmail.com>
2020-11-21 17:27:15 -0500

    fixed nav bar issues and started fonts

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,7 @@ window.onscroll = (function() {
 	if (previousScrollY > currentScrollY) {
 		$(".header").slideDown("slow");
 	} else {
-		$(".header").slideUp("slow");
+		$(".header").slideUp("fast");
 	}
 	previousScrollY = window.pageYOffset;
 });
