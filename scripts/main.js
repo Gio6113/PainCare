c7ac216c01ae1b081453fee47b254fd00b69b3f5
Younes Anys <yanys048@uottawa.ca>
2020-11-23 08:52:26 -0500

    merge

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,17 @@ window.onscroll = (function() {
 	}
 	previousScrollY = window.pageYOffset;
 });
+let navToggle = document.querySelector(".nav__toggle");
+let navWrapper = document.querySelector(".nav__wrapper");
+
+navToggle.addEventListener("click", function () {
+  if (navWrapper.classList.contains("active")) {
+    this.setAttribute("aria-expanded", "false");
+    this.setAttribute("aria-label", "menu");
+    navWrapper.classList.remove("active");
+  } else {
+    navWrapper.classList.add("active");
+    this.setAttribute("aria-label", "close menu");
+    this.setAttribute("aria-expanded", "true");
+  }
+});
