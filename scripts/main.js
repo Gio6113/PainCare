82007f47034585d334ec66e2fca34c8a339f886f
Giorgio Sawaya <giorgiosawaya@gmail.com>
2020-12-02 10:19:11 -0500

    work done on form editor

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,17 @@
-var previousScrollY = window.pageYOffset;
-window.onscroll = (function() {
-	var currentScrollY = window.pageYOffset;
-	if (previousScrollY > currentScrollY) {
-		$(".header").slideDown("slow");
-	} else {
-		$(".header").slideUp("slow");
-	}
-	previousScrollY = window.pageYOffset;
-});
-
-let navToggle = document.querySelector(".nav__toggle");
-let navWrapper = document.querySelector(".nav__wrapper");
-
-navToggle.addEventListener("click", function () {
-  if (navWrapper.classList.contains("active")) {
-    this.setAttribute("aria-expanded", "false");
-    this.setAttribute("aria-label", "menu");
-    navWrapper.classList.remove("active");
-  } else {
-    navWrapper.classList.add("active");
-    this.setAttribute("aria-label", "close menu");
-    this.setAttribute("aria-expanded", "true");
-  }
-});
+//var previousScrollY = window.pageYOffset;
+//
+//window.onscroll = (function() {
+//	var currentScrollY = window.pageYOffset;
+//	if (previousScrollY > currentScrollY) {
+//		$(".header").slideDown("slow");
+//	} else {
+//		$(".header").slideUp("slow");
+//	}
+//	previousScrollY = window.pageYOffset;
+//});
+//
+//let navToggle = document.querySelector(".nav_toggle");
+//let navWrapper = document.querySelector(".nav_wrapper");
+//
+//
+//
